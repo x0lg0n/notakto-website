297826d4a31391fbca2ae8e9e784210964f40a26
Rakshit <rakshitg600@gmail.com>
2025-06-01 00:35:50 +0530

    font set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { VT323 } from 'next/font/google'
 
 export const metadata: Metadata = {
   title: "Notakto",
@@ -11,13 +12,17 @@ export const metadata: Metadata = {
   creator: "Notakto Team",
 };
 
+const vt323 = VT323({
+  weight: "400"
+})
+ 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={vt323.className}>
       <body>
         {children}
         <Analytics/>
