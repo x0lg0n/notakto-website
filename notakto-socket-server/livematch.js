fa14396842aaa7c937b78275f695498fac23d375
Vedant terse <vedantterse75575@gmail.com>
2025-06-10 01:59:55 +0530

    use server.listen instead of app.listen

--- notakto-socket-server/livematch.js
+++ notakto-socket-server/livematch.js
@@ -98,4 +98,4 @@ function checkGameOver(grid) {
 }
 
 const PORT = 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
