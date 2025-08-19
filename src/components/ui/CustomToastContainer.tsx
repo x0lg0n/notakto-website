18baf466b7d42a79ed3190990e681ebb17a883fb
AshmitSherigar <sherigarashmit@gmail.com>
2025-08-19 14:22:02 +0530

    Fix : Added Space between each Toast

--- src/components/ui/CustomToastContainer.tsx
+++ src/components/ui/CustomToastContainer.tsx
@@ -5,8 +5,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { VT323 } from "next/font/google";
 
 const vt323 = VT323({
-  weight: "400", 
-  subsets: ["latin"],
+    weight: "400",
+    subsets: ["latin"],
 });
 
 type CustomToastContainerProps = Omit<ToastContainerProps, 'toastClassName'> & {
@@ -28,7 +28,7 @@ export function CustomToastContainer({
 }: CustomToastContainerProps = {}) {
     return (
         <ToastContainer //many more props available
-            position={position} 
+            position={position}
             autoClose={autoClose}        // Time for which the toast will be visible 
             hideProgressBar={hideProgressBar}  // progress bar for cool down will be hidden
             newestOnTop={newestOnTop}     // older toasts stay on top
@@ -37,9 +37,10 @@ export function CustomToastContainer({
             draggable={draggable}        // toast can be dragged
             pauseOnHover={pauseOnHover}     // The Toast Notification pauses on hover
             closeButton={closeButton}     // Close (X) button to manually close the toast
-            toastClassName={toastClassName||(() =>
-                `${vt323.className} text-[22px] text-center w-[300px] bg-black text-white font-mono border border-blue-500 rounded-md px-4 py-3 shadow-[0_0_12px_#00ffff] tracking-wider`
-                )}
+            toastClassName={toastClassName || (() =>
+                `${vt323.className} relative text-[22px] text-center w-[300px] bg-black text-white font-mono border border-blue-500 rounded-md px-4 py-3 shadow-[0_0_12px_#00ffff] tracking-wider mb-3`
+            )}
+            className="pb-2"
             {...rest} // Spread the rest of the props
         />
     );
